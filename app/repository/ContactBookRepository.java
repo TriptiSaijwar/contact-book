b900e497378ef037d60e93373381cd253e8b8bb9
Tripti Saijwar <tripti@Admins-MacBook-Pro-8.local>
2018-05-16 00:56:28 +0530

    using JPA.withTransaction

--- app/repository/ContactBookRepository.java
+++ app/repository/ContactBookRepository.java
@@ -2,12 +2,14 @@ package repository;
 
 import models.UserContact;
 import play.Logger;
+import play.db.jpa.JPA;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import java.util.List;
+
+//import javax.persistence.EntityManager;
+
+//import javax.persistence.EntityManager;
 
 /**
  * Created by tripti on 01/04/18.
@@ -15,30 +17,34 @@ import java.util.List;
 
 public class ContactBookRepository implements IContactBookRepository {
 
-    EntityManagerFactory entityManagerFactory;
-    EntityManager entityManager;
+//    EntityManagerFactory entityManagerFactory;
+//    EntityManager entityManager;
 
     ContactBookRepository() {
-        entityManagerFactory = Persistence.createEntityManagerFactory("defaultPersistenceUnit");
+        //root method to create EntityManager
+//        entityManagerFactory = Persistence.createEntityManagerFactory("defaultPersistenceUnit");
     }
 
     @Override
     public UserContact createUser(UserContact userContact) {
         try {
-            entityManager = entityManagerFactory.createEntityManager();
-            Query query = entityManager.createQuery("select u from UserContact u where u.email = :email");
-            query.setParameter("email",userContact.getEmail() );
-            UserContact userContactInDb = (UserContact)query.getSingleResult();
-            if (userContactInDb == null) {
-                entityManager.getTransaction().begin();
-                entityManager.persist(userContact);
-                entityManager.getTransaction().commit();
-                System.out.printf("Generated Contact ID = " + userContact.getUserId());
-                return userContact;
-            }
-            else {
-                System.out.printf("Already Persist with Contact ID = " + userContactInDb.getUserId());
-            }
+            // JPA method to create EntityManager without creating EntityManagerFactory here
+            //            entityManager = entityManagerFactory.createEntityManager();
+            return JPA.withTransaction("default",true, () -> {
+                TypedQuery<UserContact> query = JPA.em().createQuery("select u from UserContact u where u.email = :email",UserContact.class);
+                query.setParameter("email",userContact.getEmail());
+                try {
+                    UserContact userContactInDb = query.getSingleResult();
+                    System.out.printf("Already Persist with Contact ID = " + userContactInDb.getUserId());
+                    return null;
+                } catch (Exception e) {
+                    JPA.em().getTransaction().begin();
+                    JPA.em().persist(userContact);
+                    JPA.em().getTransaction().commit();
+                    System.out.printf("Generated Contact ID = " + userContact.getUserId());
+                    return userContact;
+                }
+            });
         }
         catch (Throwable throwable ) {
             Logger.error("{} {} \n{}", "error while finding user onject for", userContact.getEmail(), throwable.getCause());
@@ -48,46 +54,48 @@ public class ContactBookRepository implements IContactBookRepository {
 
     @Override
     public void deleteUser(String emailId) {
-        entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        UserContact userContact = entityManager.find(UserContact.class,emailId);
-        entityManager.remove(userContact);
-        entityManager.getTransaction().commit();
-        System.out.printf("Removed Contact ID = " + userContact.getUserId());
+//        entityManager = entityManagerFactory.createEntityManager();
+//        entityManager.getTransaction().begin();
+//        UserContact userContact = entityManager.find(UserContact.class,emailId);
+//        entityManager.remove(userContact);
+//        entityManager.getTransaction().commit();
+//        System.out.printf("Removed Contact ID = " + userContact.getUserId());
 
         return;
     }
 
     @Override
     public UserContact editUserDetails(UserContact userContact) {
-        entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        UserContact updatedContact = null;
-        try {
-            updatedContact = entityManager.merge(userContact);
-        }
-        catch (IllegalArgumentException ex) {
-            System.out.printf("Not able to Update Contact ID = " + userContact.getUserId());
-        }
-        entityManager.getTransaction().commit();
-        System.out.printf("Updated Contact ID = " + userContact.getUserId());
-
-        return updatedContact;
+//        entityManager = entityManagerFactory.createEntityManager();
+//        entityManager.getTransaction().begin();
+//        UserContact updatedContact = null;
+//        try {
+//            updatedContact = entityManager.merge(userContact);
+//        }
+//        catch (IllegalArgumentException ex) {
+//            System.out.printf("Not able to Update Contact ID = " + userContact.getUserId());
+//        }
+//        entityManager.getTransaction().commit();
+//        System.out.printf("Updated Contact ID = " + userContact.getUserId());
+//
+//        return updatedContact;
+        return null;
     }
 
     @Override
     public List<UserContact> getAllUsers() {
-        entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        List<UserContact> userContactList = entityManager.createQuery("select u from UserContact u").getResultList();
-        entityManager.getTransaction().commit();
-        if (userContactList != null) {
-            System.out.printf("No Contact Found...");
-        }
-        else {
-            System.out.printf(userContactList.size() + " Contacts Found");
-        }
-
-        return userContactList;
+//        entityManager = entityManagerFactory.createEntityManager();
+//        entityManager.getTransaction().begin();
+//        List<UserContact> userContactList = entityManager.createQuery("select u from UserContact u").getResultList();
+//        entityManager.getTransaction().commit();
+//        if (userContactList != null) {
+//            System.out.printf("No Contact Found...");
+//        }
+//        else {
+//            System.out.printf(userContactList.size() + " Contacts Found");
+//        }
+//
+//        return userContactList;
+        return null;
     }
 }
